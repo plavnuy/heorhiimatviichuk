54632d41519aba8c36ce5d9583072c2877197c06
plavnuy <heorhiim@gmail.com>
2026-02-16 22:25:14 +0200

    auto commit

--- cursor.js
+++ cursor.js
@@ -6,7 +6,7 @@ canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
 
 let config = {
-  TEXTURE_DOWNSAMPLE: 1,
+  TEXTURE_DOWNSAMPLE: 0.5,
   DENSITY_DISSIPATION: 0.98,
   VELOCITY_DISSIPATION: 0.89,
   PRESSURE_DISSIPATION: 0.2,
